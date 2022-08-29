a26e52bc78c61ea03f4670ed149feeec0cfeffb0
bakiensar <babuyi4379@gmail.com>
2022-08-30 00:43:33 +0300

    form oluşturuldu...todos lar eklenece

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 
 function App() {
   const [todoText, setTodoText] = useState('')
+  const [todos, setTodos] = useState([])
 
   const handleSubmit = (event) => {
     event.preventDefault()
@@ -9,25 +10,33 @@ function App() {
       alert("Todo can't be empty")
       return
     }
+    console.log(todoText)
   }
 
   return (
     <div className="container my-5">
       <h1 className="text-center">TODO APP</h1>
       <form onSubmit={handleSubmit}>
-        <div class="input-group mb-3">
+        <div className="input-group mb-3">
           <input
             value={todoText}
             type="text"
-            class="form-control"
+            className="form-control"
             placeholder="Add Todo"
             onChange={(event) => setTodoText(event.target.value)}
           />
-          <button class="btn btn-primary" type="submit">
+          <button className="btn btn-primary" type="submit">
             ADD
           </button>
         </div>
       </form>
+      {todos.length <= 0 ? (
+        <p className="text-center my-5">You don't have any todos yet. </p>
+      ) : (
+        <div>
+          <p>Todo will be listed</p>
+        </div>
+      )}
     </div>
   )
 }
