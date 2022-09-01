548567f5697c1014bc578cd97682478f0d806a27
bakiensar <babuyi4379@gmail.com>
2022-09-02 00:54:11 +0300

    v1

--- src/App.js
+++ src/App.js
@@ -3,6 +3,24 @@ import React, { useState } from 'react'
 function App() {
   const [todoText, setTodoText] = useState('')
   const [todos, setTodos] = useState([])
+  const [isEdit, setIsEdit] = useState(false)
+  const [willUpdateTodo, setWillUpdateTodo] = useState('')
+
+  const deleteTodo = (id) => {
+    const filteredTodo = todos.filter((item) => item.id !== id)
+    setTodos(filteredTodo)
+  }
+
+  const changeIsDone = (id) => {
+    const searchedTodo = todos.find((item) => item.id === id)
+
+    const updatedTodo = {
+      ...searchedTodo,
+      isDone: !searchedTodo.isDone,
+    }
+    const filteredTodo = todos.filter((item) => item.id !== id)
+    setTodos([updatedTodo, ...filteredTodo])
+  }
 
   const handleSubmit = (event) => {
     event.preventDefault()
@@ -10,7 +28,32 @@ function App() {
       alert("Todo can't be empty")
       return
     }
-    console.log(todoText)
+    const hasTodo = todos.find((item) => item.text === todoText)
+    if (hasTodo !== undefined) {
+      alert('You have the Todo already')
+      return
+    }
+    if (isEdit === true) {
+      const searchedTodo = todos.find((item) => item.id === willUpdateTodo)
+      const updatedTodo = {
+        ...searchedTodo,
+        text: todoText,
+      }
+      const filteredTodo = todos.filter((item) => item.id !== willUpdateTodo)
+      setTodos([updatedTodo, ...filteredTodo])
+      setTodoText('')
+      setIsEdit(false)
+      setWillUpdateTodo('')
+    } else {
+      const newTodo = {
+        id: new Date().getTime(),
+        text: todoText,
+        isDone: false,
+        date: new Date(),
+      }
+      setTodos([newTodo, ...todos])
+      setTodoText('')
+    }
   }
 
   return (
@@ -25,17 +68,52 @@ function App() {
             placeholder="Add Todo"
             onChange={(event) => setTodoText(event.target.value)}
           />
-          <button className="btn btn-primary" type="submit">
-            ADD
+          <button
+            className={`btn btn-${isEdit === true ? 'success' : 'primary'}`}
+            type="submit"
+          >
+            {isEdit === true ? 'SAVE' : 'DONE'}
           </button>
         </div>
       </form>
       {todos.length <= 0 ? (
         <p className="text-center my-5">You don't have any todos yet. </p>
       ) : (
-        <div>
-          <p>Todo will be listed</p>
-        </div>
+        <React.Fragment>
+          {todos.map((item) => (
+            <div
+              className={`alert alert-${
+                item.isDone === true ? 'danger' : 'primary'
+              } d-flex justify-content-between`}
+            >
+              <p className="my-auto">{item.text}</p>
+              <div>
+                <button
+                  onClick={() => deleteTodo(item.id)}
+                  className="btn btn-sm btn-danger"
+                >
+                  Delete
+                </button>
+                <button
+                  onClick={() => {
+                    setIsEdit(true)
+                    setWillUpdateTodo(item.id)
+                    setTodoText(item.text)
+                  }}
+                  className="btn btn-success btn-sm mx-1 "
+                >
+                  Edit
+                </button>
+                <button
+                  onClick={() => changeIsDone(item.id)}
+                  className="btn btn-sm btn-secondary"
+                >
+                  {item.isDone === false ? 'DONE' : 'UNDONE'}
+                </button>
+              </div>
+            </div>
+          ))}
+        </React.Fragment>
       )}
     </div>
   )
